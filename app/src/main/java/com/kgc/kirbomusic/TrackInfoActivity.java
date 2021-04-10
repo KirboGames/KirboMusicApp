dfb4209cdd74aea4c1fd2f9235242cb09b43054f
KirboGames <kirbo06@mail.ru>
2021-04-10 17:28:37 +0200

    clickable links

--- app/src/main/java/com/kgc/kirbomusic/TrackInfoActivity.java
+++ app/src/main/java/com/kgc/kirbomusic/TrackInfoActivity.java
@@ -3,14 +3,17 @@ package com.kgc.kirbomusic;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,6 +21,7 @@ public class TrackInfoActivity extends AppCompatActivity {
     JSONObject Track;
     Bitmap cover;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +39,8 @@ public class TrackInfoActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    private void setContent() throws JSONException{
+
+    private void setContent() throws JSONException {
         Bitmap bitmap = BitmapFactory.decodeFile(intent.getStringExtra("FilesDir") + "/Cover/" + Track.getString("cover") + ".jpg");
         Bitmap bitmap1 = BitmapFactory.decodeFile(intent.getStringExtra("FilesDir") + "/Cover/" + Track.getString("cover") + ".jpg");
         RenderScript rs = RenderScript.create(this);
@@ -51,5 +56,47 @@ public class TrackInfoActivity extends AppCompatActivity {
         ((ImageView) findViewById(R.id.track_info_track_cover_bg)).setImageBitmap(bitmap1);
         ((TextView) findViewById(R.id.track_info_track_name)).setText(Track.getString("name"));
         ((TextView) findViewById(R.id.about_track)).setText(Track.getString("about"));
+
+        ((CardView) findViewById(R.id.telegram_link)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Track.getString("Telegram"))));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ((CardView) findViewById(R.id.vk_link)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Track.getString("VK"))));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ((CardView) findViewById(R.id.youtube_link)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Track.getString("Youtube"))));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ((CardView) findViewById(R.id.spotify_link)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Track.getString("Spotify"))));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
